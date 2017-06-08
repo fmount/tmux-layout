8bcbed85184bb890a2997dcc2d168738b2cb7dd7
fmount <francesco.pantano@linux.com>
2017-06-08 17:26:31 +0200

    Fix default path in config.py

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,7 @@
 
 # possible locations for the configuration file placement on the local filesystem
 parameters_json_file_source = {
-	'abs': "~/git/tmux-layout-plugin/config/parameters.json",
+	'abs': "~/.tmux/plugins/tmux-layout/config/parameters.json",
 	#'local': "~/.config/parameters.json",
 	#'system-wide': "/etc/parameters.json"
 }
